355728dbc45010fcc862ecf26c283313dbf879fa
@hexlet/cli <support@hexlet.io>
2023-09-17 17:15:22 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -7,7 +7,7 @@ import java.util.List;
 // BEGIN
 public class App {
     public static Boolean scrabble(String letters, String word)  {
-        String[] lettersArr = letters;
+        String[] lettersArr = letters.split("");
         List<String> lettersList = Arrays.asList(lettersArr);
         var i = 0;
         while (i < word.length()) {
