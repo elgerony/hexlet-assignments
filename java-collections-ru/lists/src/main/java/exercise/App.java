6e447534dbcc504bcc98d2701f81bebf2b8ace2e
@hexlet/cli <support@hexlet.io>
2023-09-20 13:25:16 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -9,13 +9,14 @@ public class App {
     public static Boolean scrabble(String letters, String word)  {
         String[] lettersArr = letters.split("");
         String[] wordArr = word.toLowerCase().split("");
-        List<String> lettersList = Arrays.asList(lettersArr);
-        List<String> wordList = Arrays.asList(word);
+        List<String> lettersList = new ArrayList<>(Arrays.asList(lettersArr));
+        List<String> wordList = new ArrayList<>(Arrays.asList(wordArr));
         var i = 0;
         while (i < wordList.size()) {
             if (!lettersList.contains(wordList.get(i))) {
                 return false;
             }
+            lettersList.remove(wordList.get(i));
             i++;
         }
         return true;
